8b275f4a5740e12e2a8458f9140a9258b106c04e
Student <lab-208@aptech-msg.edu>
2023-04-05 15:47:50 +0500

    nexuss

---
+++ project/pages/home/index.js
@@ -0,0 +1,5 @@
+// var navBtnIcon = document.getElementById("navBtnIcon");
+// var navbarId = document.getElementById("navbarId");
+// navBtnIcon.addEventListener("click", () => {
+//   navbarId.classList.add("active");
+// });
